90032ac3af8a81a5e211636c8ece7de9b16d2ef3
3465h <129896279+3465h@users.noreply.github.com>
2026-02-08 19:38:01 +0300

    Add socket server for chat functionality
    Refactor EchoBot to implement a socket server for chat functionality.

--- main2.py
+++ main2.py
@@ -1,27 +1,51 @@
-import logging
-import asyncio
-from slixmpp import ClientXMPP
+import socket
+import threading
 
-class EchoBot(ClientXMPP):
-    def __init__(self, jid, password):
-        super().__init__(jid, password)
-        self.add_event_handler("session_start", self.start)
-        self.add_event_handler("message", self.message)
+# Настройки
+HOST = '0.0.0.0'  # Слушаем все интерфейсы
+PORT = 55555
 
-    async def start(self, event):
-        self.send_presence()
-        await self.get_roster()
-        print("Бот в сети! Можно писать на admin@10.0.0.12")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
 
-    def message(self, msg):
-        if msg['type'] in ('chat', 'normal'):
-            msg.reply(f"Эхо: {msg['body']}").send()
+clients = []
+nicknames = []
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(levelname)-8s %(message)s')
-    # Проверь, что тут твой IP
-    xmpp = EchoBot('admin@10.0.0.12', 'password')
-    
-    # Вот тут самая важная часть для работы в новых версиях:
-    xmpp.connect()
-    xmpp.process(forever=True)
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} ливнул!'.encode('utf-8'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    print(f"Сервер запущен на порту {PORT}...")
+    while True:
+        client, address = server.accept()
+        print(f"Коннект с {str(address)}")
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Никнейм клиента: {nickname}")
+        broadcast(f"{nickname} зашел в чат!".encode('utf-8'))
+        client.send('Подключено к серверу!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
